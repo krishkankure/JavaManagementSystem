1ed34fe76c7813b53c12a7165bf0a388594d955d
sm2006 <krishkankure6@gmail.com>
2022-01-03 17:32:08 -0500

    edits

--- Load.java
+++ Load.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Load {
 	Scanner s = new Scanner(System.in);
 	String userName;
-	public void takeInfo(){
+	public void takeInfo(){ // takes user info and adds to hashmap function yet to be created
 		System.out.println("Enter your username");
 		userName = s.nextLine();		
 	}
