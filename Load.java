5d29d37d6da76056c36f6566f223d786d019378b
sm2006 <krishkankure6@gmail.com>
2022-01-03 18:26:35 -0500

    seperated off the the takeInfo section into multiple sections; mainmenu() for top of program funcitonality

--- Load.java
+++ Load.java
@@ -5,18 +5,29 @@ public class Load {
 	Scanner s = new Scanner(System.in);
 	String userName;
 	String jobTitle;
-	HashMap<String, String> member = new HashMap<String, String>(); // matches string key (userName) to boolean (member or not);
 	HashMap<String, String> jobs = new HashMap<String, String>(); // creates new hashmap with string key and string value ()
-	public void takeInfo(){ // takes user info
-		System.out.println("Enter your username");
-		userName = s.nextLine();
-		System.out.println("Enter your job title.?");
-		jobTitle = s.nextLine();
-		storeJob(userName, jobTitle);
+	public void mainMenu(){ // takes user info
+		System.out.println("What would you like to do?");
+		// create a while loop that holds this under a boolean (create it) running.
+		// make it so this stuff is repeatable
 		
 	}
 	public void storeJob(String u, String j) { // passes through u (userName) and j (jobTitle)
-		jobs.put(u, j);
+		jobs.put(u, j); //enters the key (username) and the value (job)
 		System.out.println("Registered profession '" + jobs.get(userName) + "' under user '" + u +"'.");
 	}
+	public void userCheck(String u) { // checks if there is already a value in the Hashmap 
+		
+	}
+	public void takeJob() {
+		System.out.println("Enter your job title.?");
+		jobTitle = s.nextLine();
+		storeJob(userName, jobTitle);
+	}
+	public void takeName() {
+		System.out.println("Enter your username");
+		userName = s.nextLine();
+		userCheck(userName);
+	}
+
 }
