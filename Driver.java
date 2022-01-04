5625dbb34cc9b115f805e491807709adaa7eca7c
sm2006 <krishkankure6@gmail.com>
2022-01-04 09:59:40 -0500

    Created basic GUI no functionality or list object

--- Driver.java
+++ Driver.java
@@ -2,7 +2,8 @@ package mgmt;
 
 public class Driver {
 	public static void main(String[] args) {
-		Load l = new Load();
-		l.mainMenu();
+		// Load l = new Load();
+		MainGUI m = new MainGUI();
+		m.mainScreen();
 	}
 }
---
+++ MainGUI.java
@@ -0,0 +1,21 @@
+package mgmt;
+import javax.swing.*;
+public class MainGUI {
+    public void mainScreen() {
+        JLabel l=new JLabel("Welcome to the Kankure Management System");
+        JFrame f=new JFrame("Kankure Management System");  
+        JButton n=new JButton("New User"); 
+        JButton c=new JButton("Clear all users");
+        n.setBounds(50,100,95,30);
+        c.setBounds(350,100,95,30);
+        l.setBounds(115, 50, 300, 30);
+        f.add(n); 
+        f.add(c); 
+        f.add(l); 
+        f.setSize(500,500);  
+        f.setLayout(null);  
+        f.setVisible(true); 
+
+        
+    }
+}
