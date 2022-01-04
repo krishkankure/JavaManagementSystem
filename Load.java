d3fdad1667f41afb1aa8a30c3f26011b23983b73
sm2006 <krishkankure6@gmail.com>
2022-01-04 16:41:52 -0500

    Removed Marriage; Added Removal Feature

--- Load.java
+++ Load.java
@@ -14,8 +14,6 @@ public class Load {
 	String edu;
 	String city;
 	String age;
-	String marriage;
-	Boolean married;
 	Integer dummyId;
 	String stdin;
 	HashMap<Integer, String> names = new HashMap<Integer, String>(); // creates new hashmap with string key and string value ()
@@ -23,13 +21,19 @@ public class Load {
 	HashMap<Integer, String> jobs = new HashMap<Integer, String>(); // creates new hashmap with string key and string value ()
 	HashMap<Integer, String> edus = new HashMap<Integer, String>(); // creates new hashmap with string key and string value ()
 	HashMap<Integer, String> ages = new HashMap<Integer, String>(); // creates new hashmap with string key and string value ()
-	HashMap<Integer, Boolean> marriageStatus = new HashMap<Integer, Boolean>(); // creates new hashmap with string key and string value ()
 	final static String outputFilePath = "data/krishDB.txt";
+	final static String filePath = "data/krishDB.txt";
 	File file = new File(outputFilePath); // created new way to print name and id to a file just edit so it has file for each hash read java for geeks article
 	public void mainMenu(){ // takes user info
+		readData();
 		System.out.println("| 1 = New User | 2 = Find User | 3 = Print All Users | 4 Delete a user | 5 Clear all users |");
 		while(running) {
-			save();
+			save("data/nameDB.txt", names);
+			save("data/jobDB.txt", names);
+			save("data/cityDB.txt", names);
+			save("data/eduDB.txt", names);
+			save("data/ageDB.txt", names);
+			save("data/marriageDB.txt", names);
 			System.out.println(" ");
 			System.out.print("> ");
 			stdin = s.nextLine();
@@ -68,14 +72,6 @@ public class Load {
 		edu = s.nextLine();
 		System.out.println("How old are you?");
 		age = s.nextLine();
-		System.out.println("Are you currently married y/n?");
-		marriage = s.nextLine();
-		if(marriage.equalsIgnoreCase("y")){
-			married = true;
-		}
-		else {
-			married = false;
-		}
 		dummyId = idGen();
 		while(names.containsKey(dummyId)) {
 			dummyId = idGen();
@@ -85,7 +81,6 @@ public class Load {
 		jobs.put(dummyId, job);
 		edus.put(dummyId, edu);
 		ages.put(dummyId, age);
-		marriageStatus.put(dummyId, married);
 		System.out.println("Created New User under id (" + dummyId + ")");
 	}
 	public int idGen() {
@@ -98,7 +93,6 @@ public class Load {
 			System.out.println("Current Job: " + jobs.get(i));
 			System.out.println("Education: " + edus.get(i));
 			System.out.println("Age: " + ages.get(i));
-			System.out.println("Maritial Status:" + marriageStatus.get(i));
 		}
 		else {
 			System.out.println("No such user exists under that id");
@@ -108,7 +102,7 @@ public class Load {
 		System.out.println("All Registered Users: ");
 		for(int i = 0; i < 500; i++) {
 			if(names.get(i) != null) {
-				System.out.println(names.get(i) + ", " + ages.get(i));
+				System.out.println(names.get(i) + " (" + i + ")");
 			}
 		}
 	}
@@ -117,13 +111,12 @@ public class Load {
 		int iPass = s.nextInt();
 		Integer iPassed = Integer.valueOf(iPass);
 		if(names.get(iPassed) != null) {
-			System.out.println("Deleted user" + names.get(iPassed) + " (" + iPassed + ")");
+			System.out.println("Deleted user " + names.get(iPassed) + " (" + iPassed + ")");
 			names.remove(iPassed);
 			jobs.remove(iPassed);
 			cities.remove(iPassed);
 			edus.remove(iPassed);
 			ages.remove(iPassed);
-			marriageStatus.remove(iPassed);
 		}
 		else {
 			System.out.println("No such user exists under that id");
@@ -133,18 +126,17 @@ public class Load {
 		System.out.println("Deleting All Registered Users: ");
 		for(int i = 0; i < 500; i++) {
 			if(names.get(i) != null) {
-				System.out.println("Deleting" + names.get(i) + ", " + ages.get(i));
+				System.out.println("Deleting " + names.get(i) + " (" + i + ")");
 				names.remove(i);
 				jobs.remove(i);
 				cities.remove(i);
 				edus.remove(i);
 				ages.remove(i);
-				marriageStatus.remove(i);
 			}
 		}
 	}
-	public void save() {
-		File file = new File(outputFilePath);
+	public void save(String path, HashMap<Integer, String> i) {
+		File file = new File(path);
   
         BufferedWriter bf = null;
   
@@ -155,7 +147,7 @@ public class Load {
   
             // iterate map entries
             for (Map.Entry<Integer, String> entry :
-                 names.entrySet()) {
+                 i.entrySet()) {
   
                 // put key and value separated by a colon
                 bf.write(entry.getKey() + ":"
@@ -164,7 +156,7 @@ public class Load {
                 // new line
                 bf.newLine();
             }
-  
+			
             bf.flush();
         }
         catch (IOException e) {
@@ -178,6 +170,50 @@ public class Load {
                 bf.close();
             }
             catch (Exception e) {
+            }
+        }
+	}
+	public void readData() {
+		BufferedReader br = null;
+  
+        try {
+  
+            // create file object
+            File file = new File(filePath);
+  
+            // create BufferedReader object from the File
+            br = new BufferedReader(new FileReader(file));
+  
+            String line = null;
+  
+            // read file line by line
+            while ((line = br.readLine()) != null) {
+  
+                // split the line by :
+                String[] parts = line.split(":");
+  
+                // first part is name, second is number
+                Integer num = Integer.parseInt(parts[0].trim());
+                String namef = parts[1].trim();
+  
+                // put name, number in HashMap if they are
+                // not empty
+                if (num != null && !namef.equals(""))
+                    names.put(num, namef);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+  
+            // Always close the BufferedReader
+            if (br != null) {
+                try {
+                    br.close();
+                }
+                catch (Exception e) {
+                };
             }
         }
 	}
