b71ca2df9ef734781f96d20dd3c363707bb89309
sm2006 <krishkankure6@gmail.com>
2022-01-12 08:31:48 -0500

    deleted user; remove name search for now

--- Driver.java
+++ Driver.java
@@ -3,6 +3,7 @@ public class Driver {
 	public static void main(String[] args) {
 		Load l = new Load();
 		MainGUI m = new MainGUI();
-		m.mainScreen();
+		// m.mainScreen();
+		l.mainMenu();
 	}	
 }
